27dc0affe50ae3f87d690e7b164025fced77b9ab
donevswe <donevswe@gmail.com>
2022-05-14 20:53:29 +0200

    8

---
+++ Lectures_Effective_Python_Programming/Spyder_test_debbug_code.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def hi(first, last):
+    var=12
+    print('Hello {} {}'.format(first,last))
+    var=15
+    sum1=var+255
+    
+hi('Evgeniy', 'Donev')
